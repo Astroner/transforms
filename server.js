d292b51a5f2a1ac08d9791699e3cc36d106f8702
Astroner <zhenyanikforov@gmail.com>
2020-10-30 23:22:20 +0300

    custom server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+
+const app = next({ dev })
+
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+    const server = express();
+
+    server.get("*", handle);
+
+    server.listen(process.env.PORT || 3000, process.env.HOST || "localhost", () => {
+        console.log(`
+Server started with arguments:
+    HOST: ${process.env.HOST || "localhost"}
+    PORT: ${process.env.PORT || 3000}`)
+    })
+})
