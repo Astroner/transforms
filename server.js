e92ef6659ccd437190abb420b4213e180deda7d2
Astroner <zhenyanikforov@gmail.com>
2020-10-30 23:46:12 +0300

    cs

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require("express");
-const next = require('next')
+const next = require("next");
 
-const dev = process.env.NODE_ENV !== 'production'
-
-const app = next({ dev })
-
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const host = process.env.HOST || "0.0.0.0";
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-    const server = express();
+  const server = express();
 
-    server.get("*", (req, res) => handle(req, res));
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
 
-    server.listen(process.env.PORT || 3000, process.env.HOST || "localhost", () => {
-        console.log(`
-Server started with arguments:
-    HOST: ${process.env.HOST || "localhost"}
-    PORT: ${process.env.PORT || 3000}`)
-    })
-})
+  server.listen(port, host, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on ${host}:${port}`);
+  });
+});
